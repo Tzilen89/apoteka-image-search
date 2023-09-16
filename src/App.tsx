18b4a8f0a14b7b238d819fe0917044389b38154b
Thomas Zilen Kjær <thokj@nuuday.dk>
2023-09-16 10:19:04 +0200

    added debouncing, to reduce api calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import debounce from 'lodash/debounce';
 import ImageGridComponent from './components/ImageGridComponent';
 
 function App() {
   const [currentSearch, setCurrentSearch] = useState<string>('');
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState<string>(currentSearch);
+
+  const debouncedSetSearchTerm = useCallback(
+    debounce((value: string) => setDebouncedSearchTerm(value), 300),
+    []
+  );
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setCurrentSearch(value);
+    debouncedSetSearchTerm(value);
+  };
 
   return (
     <div className="App">
@@ -13,12 +26,12 @@ function App() {
         <input
           type="text"
           value={currentSearch}
-          onChange={e => setCurrentSearch(e.target.value)}
+          onChange={handleSearchChange}
           placeholder="Search for images..."
         />
       </div>
       
-      <ImageGridComponent searchTerm={currentSearch} onSelect={setSelectedImage}/>
+      <ImageGridComponent searchTerm={debouncedSearchTerm} onSelect={setSelectedImage}/>
       {selectedImage && (
         <div className="large-image-view" onClick={() => setSelectedImage(null)}>
           <img src={selectedImage} alt="Selected" />
