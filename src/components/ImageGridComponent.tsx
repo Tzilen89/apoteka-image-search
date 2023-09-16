571afb437eef1ff6112c0526187417f2cca6f2b9
Thomas Zilen Kjær <thokj@nuuday.dk>
2023-09-16 12:26:49 +0200

    an attempt at loading screen

--- src/components/ImageGridComponent.tsx
+++ src/components/ImageGridComponent.tsx
@@ -13,30 +13,41 @@ interface ImageGridProps {
 
 const ImageGridComponent: React.FC<ImageGridProps> = ({ searchTerm, onSelect }) => {
   const [images, setImages] = useState<Image[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
+    setLoading(true);
+    setError(null);
     const fetchImages = async () => {
-      try {
-        const fetchedImages = await getFlickrImages(searchTerm, 27);
-        setImages(fetchedImages);
-        setError(null);
-      } catch (error) {
-        setError('Failed to fetch images from Flickr. Please check your internet connection or try again later.');
-      }
-    };
+      getFlickrImages(searchTerm, 27)
+        .then(fetchedImages => {
+          setImages(fetchedImages);
+          setError(null);
+        })
+        .catch(error => {
+          setError('Failed to fetch images from Flickr. Please check your internet connection or try again later.');
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
 
     fetchImages();
   }, [searchTerm]);
 
   return (
     <div>
+      {loading && <div>loading..</div>}
       {error && <div className="error-message">{error}</div>}
       <div className="image-grid">
         {images.map((image, index) => (
           <div key={index} className="image-container">
             <img src={image.url} onClick={() => onSelect(image.url)} />
           </div>
+        ))}
+        {loading && Array(27).fill(0).map((_, index) => (
+          <div key={index} className="image-placeholder"></div>
         ))}
       </div>
     </div>
